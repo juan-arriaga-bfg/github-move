d9bd2008522ec305fbd00966587566c7c1e571f5
OlegDolmatovih <oleg@neskinsoft.com>
2018-04-27 19:04:46 +0300

    fix add coins in coin piece

--- Assets/Scripts/ProjectScripts/GameLogic/BoardComponents/TouchReactionDefinitionComponent/TouchReactionDefinitionCollectResource.cs
+++ Assets/Scripts/ProjectScripts/GameLogic/BoardComponents/TouchReactionDefinitionComponent/TouchReactionDefinitionCollectResource.cs
@@ -7,36 +7,14 @@ public class TouchReactionDefinitionCollectResource : TouchReactionDefinitionCom
         var storage = piece.GetComponent<ResourceStorageComponent>(ResourceStorageComponent.ComponentGuid);
 
         if (storage == null || storage.Resources == null || storage.Resources.Amount == 0) return false;
+
+        AddResourceView.Show(position, storage.Resources);
         
-        var shopItem = new ShopItem
+        piece.Context.ActionExecutor.AddAction(new CollapsePieceToAction
         {
-            Uid = string.Format("add.test.{0}.{1}", storage.Resources.Currency, storage.Resources.Amount), 
-            ItemUid = storage.Resources.Currency, 
-            Amount = storage.Resources.Amount,
-            CurrentPrices = new List<Price>
-            {
-                new Price{Currency = Currency.Cash.Name, DefaultPriceAmount = 0}
-            }
-        };
-        
-        ShopService.Current.PurchaseItem
-        ( 
-            shopItem,
-            (item, s) =>
-            {
-                // on purchase success
-                piece.Context.ActionExecutor.AddAction(new CollapsePieceToAction
-                {
-                    To = position,
-                    Positions = new List<BoardPosition>{position}
-                });
-            },
-            item =>
-            {
-                // on purchase failed (not enough cash)
-            }
-        );
-        
+            To = position,
+            Positions = new List<BoardPosition>{position}
+        });
         return true;
     }
 }
