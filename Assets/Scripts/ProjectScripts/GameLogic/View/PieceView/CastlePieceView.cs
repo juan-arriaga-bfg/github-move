48f739c59eb7ae8e7297b81bb9721c8f98903e1d
OlegDolmatovih <oleg@neskinsoft.com>
2018-06-05 20:32:24 +0300

    up castle

--- Assets/Scripts/ProjectScripts/GameLogic/View/PieceView/CastlePieceView.cs
+++ Assets/Scripts/ProjectScripts/GameLogic/View/PieceView/CastlePieceView.cs
@@ -3,6 +3,13 @@
 public class CastlePieceView : PieceBoardElementView
 {
     [SerializeField] private GameObject chest;
+    [SerializeField] private Transform cap;
+    
+    [SerializeField] private GameObject shine;
+    
+    [SerializeField] private float open;
+    [SerializeField] private float close;
+    
     private StorageComponent storage;
     
     public override void Init(BoardRenderer context, Piece piece)
@@ -13,7 +20,7 @@ public class CastlePieceView : PieceBoardElementView
         
         if (storage == null) return;
         
-        storage.Timer.OnStart += Change;
+        storage.Timer.OnStart += OnStart;
         storage.Timer.OnComplete += Change;
         
         Change();
@@ -25,13 +32,35 @@ public class CastlePieceView : PieceBoardElementView
 
         if (storage == null) return;
         
-        storage.Timer.OnStart -= Change;
+        storage.Timer.OnStart -= OnStart;
         storage.Timer.OnComplete -= Change;
+        storage.Timer.OnExecute -= OnExecute;
+    }
+
+    private void OnStart()
+    {
+        Change();
+        
+        if (storage.Filling > 0) return;
+        
+        chest.SetActive(false);
+        storage.Timer.OnExecute += OnExecute;
+    }
+
+    private void OnExecute()
+    {
+        if(storage.Timer.GetTime().TotalSeconds < 3) return;
+        storage.Timer.OnExecute -= OnExecute;
+        Change();
     }
 
     private void Change()
     {
-        chest.SetActive(storage.Filling > 0);
+        var isOpen = storage.Filling > 0;
+        
+        shine.SetActive(isOpen);
+        cap.localPosition = new Vector3(cap.localPosition.x, isOpen ? open : close);
+        chest.SetActive(true);
     }
 
     public Vector3 GetSpawnPosition()
