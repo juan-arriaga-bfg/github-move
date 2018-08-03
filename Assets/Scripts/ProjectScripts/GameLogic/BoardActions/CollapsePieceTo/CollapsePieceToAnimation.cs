5f3682aa762a16c774606b9e2fa3e4e53b4dab26
Danila Zhelenok <daniel@neskinsoft.com>
2018-08-03 17:41:47 +0300

    match animation v3

--- Assets/Scripts/ProjectScripts/GameLogic/BoardActions/CollapsePieceTo/CollapsePieceToAnimation.cs
+++ Assets/Scripts/ProjectScripts/GameLogic/BoardActions/CollapsePieceTo/CollapsePieceToAnimation.cs
@@ -42,15 +42,25 @@ public class MatchPieceToAnimation : BoardAnimation
 		var to = context.Context.BoardDef.GetPiecePosition(Action.To.X, Action.To.Y);
 		
 		var sequence = DOTween.Sequence().SetId(animationUid);
+		var elementOffset = 0.05f;
 		//sequence.timeScale = 0.5f;
-		var particlePosition = new BoardPosition(Action.To.X, Action.To.Y, 2);
+		var particlePosition = new BoardPosition(Action.To.X, Action.To.Y, 0);
 		sequence.timeScale = 1.2f;
-		sequence.InsertCallback(0.1f, () => ParticleView.Show(R.MergeParticleSystem, particlePosition));
+		sequence.InsertCallback(0.0f, () => ParticleView.Show(R.MergeParticleSystem, particlePosition));
 		for (int i = 0; i < points.Count; i++)
 		{
 			var boardElement = context.GetElementAt(points[i]);
-			sequence.Insert(0, boardElement.CachedTransform.DOJump(new Vector3(to.x, to.y, boardElement.CachedTransform.position.z), 0.5f, 1, 0.25f));
-			sequence.Insert(0.20f, boardElement.CachedTransform.DOScale(Vector3.zero, 0.20f));
+			if (points[i].Equals(Action.To))
+			{
+				sequence.Insert(0.15f, boardElement.CachedTransform.DOScale(Vector3.one * 1.2f, 0.20f));
+				//sequence.Insert(0.35f, boardElement.CachedTransform.DOScale(Vector3.zero, 0.1f));
+				sequence.Insert(0.35f + points.Count * elementOffset, boardElement.CachedTransform.DOScale(Vector3.zero, 0.1f));
+				boardElement.SyncRendererLayers(new BoardPosition(Action.To.X, Action.To.Y, 4));
+				continue;
+			}
+			
+			sequence.Insert(0 + elementOffset*i, boardElement.CachedTransform.DOMove(new Vector3(to.x, to.y, boardElement.CachedTransform.position.z), 0.25f));
+			sequence.Insert(0.20f + elementOffset*i, boardElement.CachedTransform.DOScale(Vector3.zero, 0.20f));
 			
 		}
 		
