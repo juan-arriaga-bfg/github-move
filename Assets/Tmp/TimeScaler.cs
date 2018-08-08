d94f6937d17bde8845408938bcadb645457a0d89
Danila Zhelenok <daniel@neskinsoft.com>
2018-08-08 18:05:12 +0300

    add timescaler for debug

---
+++ Assets/Tmp/TimeScaler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeScaler : MonoBehaviour
+{
+
+	[SerializeField] private Slider slider;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		slider = slider ?? GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (slider != null)
+			Time.timeScale = slider.value;
+	}
+}
