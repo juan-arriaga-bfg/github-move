d15b40333fb65d13dc5f380ee84de676d333758e
OlegDolmatovih <oleg@neskinsoft.com>
2018-08-09 17:43:50 +0300

    up hint by energy

--- Assets/Scripts/ProjectScripts/GameLogic/View/HintView/HintArrowView.cs
+++ Assets/Scripts/ProjectScripts/GameLogic/View/HintView/HintArrowView.cs
@@ -4,42 +4,41 @@ using UnityEngine;
 public class HintArrowView : BoardElementView
 {
     [SerializeField] private SpriteRenderer icon;
-
+    
+    private const float duration = 5f;
+    
     private void Show()
     {
-        float duration = 5f;
-        
         DOTween.Kill(animationUid);
         
         var sequence = DOTween.Sequence().SetId(animationUid);
+        
         sequence.Insert(0, icon.DOFade(1, 1f));
         sequence.Insert(duration * 0.5f, icon.DOFade(0, duration * 0.5f));
         
         DestroyOnBoard(duration);
     }
 
-    public static void Show(BoardPosition position, float offsetX = 0, float offsetY = 0)
+    public static void Show(BoardPosition position, float offsetX = 0, float offsetY = 0, bool focus = true)
     {
         var board = BoardService.Current.GetBoardById(0);
-        
         var target = board.BoardLogic.GetPieceAt(position);
 
-        if (target != null)
-        {
-            var multi = target.GetComponent<MulticellularPieceBoardObserver>(MulticellularPieceBoardObserver.ComponentGuid);
+        var multi = target?.GetComponent<MulticellularPieceBoardObserver>(MulticellularPieceBoardObserver.ComponentGuid);
 
-            if (multi != null)
-            {
-                position = multi.GetTopPosition;
-            }
+        if (multi != null)
+        {
+            position = multi.GetTopPosition;
         }
-        
-        var worldPos = board.BoardDef.GetSectorCenterWorldPosition(position.X, position.Up.Y, position.Z);
+
         var arrowView = board.RendererContext.CreateBoardElementAt<HintArrowView>(R.HintArrow, position);
 
         arrowView.CachedTransform.localPosition = arrowView.CachedTransform.localPosition + (Vector3.up * 2) + new Vector3(offsetX, offsetY);
         arrowView.Show();
         
+        if(focus == false) return;
+        
+        var worldPos = board.BoardDef.GetSectorCenterWorldPosition(position.X, position.Up.Y, position.Z);
         board.Manipulator.CameraManipulator.MoveTo(worldPos);
     }
 }
