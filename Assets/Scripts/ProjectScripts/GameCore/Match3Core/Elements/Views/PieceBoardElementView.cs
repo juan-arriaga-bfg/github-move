18dad3f534574626882b6e920610215f27d1aaf5
Danila Zhelenok <daniel@neskinsoft.com>
2018-08-10 16:01:07 +0300

    change drag color

--- Assets/Scripts/ProjectScripts/GameCore/Match3Core/Elements/Views/PieceBoardElementView.cs
+++ Assets/Scripts/ProjectScripts/GameCore/Match3Core/Elements/Views/PieceBoardElementView.cs
@@ -30,8 +30,8 @@ public class PieceBoardElementView : BoardElementView
     
     private SpriteRenderer selectionSprite;
     private Color baseColor = new Color(0.6f, 0.4f, 0.2f);
-    private Color dragErrorColor = new Color(0.69f, 0.2f, 0.2f);
-    private Color dragSpriteErrorColor = new Color(1f, 0.3f, 0.3f, 0.9f);
+    private Color dragErrorColor = new Color(0.7f, 0.1f, 0.1f);
+    private Color dragSpriteErrorColor = new Color(1f, 0.44f, 0.44f, 0.9f);
     
     public virtual void Init(BoardRenderer context, Piece piece)
     {
