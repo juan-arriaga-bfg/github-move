93fe689ad0aa79e62d43c58bea6a2369c1592466
OlegDolmatovih <oleg@neskinsoft.com>
2018-08-16 21:39:27 +0300

    add confirm message

--- Assets/Scripts/IWUI/BankWindow/UIResourcePanelViewController.cs
+++ Assets/Scripts/IWUI/BankWindow/UIResourcePanelViewController.cs
@@ -5,6 +5,12 @@ public class UIResourcePanelViewController : UIGenericResourcePanelViewControlle
     public void DebugCurrentResources()
     {
         BoardService.Current.GetBoardById(0)?.BoardEvents.RaiseEvent(GameEventsCodes.ClosePieceUI, this);
-        CurrencyHellper.Purchase(itemUid, itemUid == Currency.Crystals.Name ? 5 : 100, null, new Vector2(Screen.width/2, Screen.height/2));
+        
+        var product = new CurrencyPair{Currency = itemUid, Amount = itemUid == Currency.Crystals.Name ? 5 : 100};
+        
+        UIMessageWindowController.CreateDefaultMessage($"You want to get {product.ToStringIcon(false)}?", () =>
+        {
+            CurrencyHellper.Purchase(product, null, new Vector2(Screen.width/2, Screen.height/2));
+        });
     }
 }
