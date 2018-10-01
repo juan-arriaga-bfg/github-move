c3a196a710895101a30a7f8456e5b9fd798be05f
Alex Sorokin <alex@neskinsoft.com>
2018-10-01 17:01:39 +0300

    Del old quests system

--- Assets/Scripts/ProjectScripts/BusinessLogic/GameData/Quests/Old/IQuestConditionOld.cs
+++
@@ -1,4 +0,0 @@
-﻿public interface IQuestConditionOld
-{
-    bool Check();
-}
--- Assets/Scripts/ProjectScripts/BusinessLogic/GameData/Quests/Old/QuestConditionOtherQuest.cs
+++
@@ -1,9 +0,0 @@
-﻿public class QuestConditionOtherQuest : IQuestConditionOld
-{
-	public int OtherUid { get; set; }
-	
-	public bool Check()
-	{
-		return GameDataService.Current.QuestsManagerOld.IsCompleted(OtherUid);
-	}
-}
