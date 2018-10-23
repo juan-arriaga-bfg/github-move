9b5148436d40e61ff6970f2f3a3b5302958b8bd6
kwelifin <daniel@neskinsoft.com>
2018-10-23 18:57:07 +0300

    resolution independent scale

--- Assets/Scripts/CameraScroll.cs
+++ Assets/Scripts/CameraScroll.cs
@@ -1,9 +1,16 @@
-﻿using Lean.Touch;
+﻿using System;
+using Lean.Touch;
 using UnityEngine;
 
 public class CameraScroll : MonoBehaviour
 {
     [SerializeField] private CameraManipulator cameraManipulator;
+
+    [Header("Interface offset")] 
+    [SerializeField] private float leftOffset;
+    [SerializeField] private float topOffset;
+    [SerializeField] private float rightOffset;
+    [SerializeField] private float bottomOffset;
     
     private int border;
     private float speed = 60f;
@@ -22,16 +29,31 @@ public class CameraScroll : MonoBehaviour
     public void ScaleClampRect()
     {
         var clamp = cameraManipulator.CurrentCameraSettings.CameraClampRegion;
-        var offsetSize = 2;
-        var CameraZoom = cameraManipulator.CameraZoom;
-        var targetZoom = cameraManipulator.TargetCameraZoom;
-        if (targetZoom < CameraZoom.ZoomMin)
-            targetZoom = CameraZoom.ZoomMin;
-        var scaleFactor = (1 + (targetZoom - CameraZoom.ZoomMin) / (CameraZoom.ZoomMax - CameraZoom.ZoomMin)) * offsetSize;
-        clamp.size = new Vector2(UnscaledRectSize.x + scaleFactor, UnscaledRectSize.y + scaleFactor);
+        var resolutionFactor = 1;
+        var camera = Camera.main;
 
-        clamp.position = new Vector2(UnscaledRectOffset.x - scaleFactor / 2,
-            UnscaledRectOffset.y - scaleFactor / 2);
+        //float leftOffset = 0.03f * 2 + 0.08f;
+        //float topOffset = 0.08f;
+        //float rightOffset = 0.03f*2 + 0.08f;
+        //float bottomOffset = 0.06f ;
+        
+        //var targetZoom = Mathf.Clamp(cameraManipulator.TargetCameraZoom, cameraZoom.ZoomMin, cameraZoom.ZoomMax);
+
+        Func<float, float> convertFunc = (vect) =>
+            Vector3.Distance(camera.ViewportToWorldPoint(new Vector3(vect, 0, 0)),
+                camera.ViewportToWorldPoint(Vector3.zero));
+        
+        var leftResult = convertFunc(leftOffset);
+        var topResult = convertFunc(topOffset);
+        var rightResult = convertFunc(rightOffset);
+        var bottomResult =  convertFunc(bottomOffset);
+       //var resultScale = (1 + (targetZoom - cameraZoom.ZoomMin) / (cameraZoom.ZoomMax - cameraZoom.ZoomMin)) * resolutionFactor;
+        clamp.size = new Vector2(UnscaledRectSize.x + leftResult + rightResult, UnscaledRectSize.y + bottomResult + topResult);
+
+        clamp.position = new Vector2(
+            UnscaledRectOffset.x - leftResult,
+            UnscaledRectOffset.y - bottomResult
+        );
             
         cameraManipulator.CurrentCameraSettings.CameraClampRegion = clamp;
     }
@@ -45,6 +67,8 @@ public class CameraScroll : MonoBehaviour
 
     private void LateUpdate()
     {
+//        var point = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+//        Debug.LogError($"{point.x:F}.{point.y:F}.{point.z:F}");
         if (lastCheckedScale != cameraManipulator.TargetCameraZoom)
         {
             ScaleClampRect();
