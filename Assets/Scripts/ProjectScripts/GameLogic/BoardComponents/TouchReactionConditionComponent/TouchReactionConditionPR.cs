e3c69bd015eeb9cb4a74d2943bbde230e3f60095
OlegDolmatovih <oleg@neskinsoft.com>
2018-11-14 14:15:13 +0300

    fix PR pieces lock

--- Assets/Scripts/ProjectScripts/GameLogic/BoardComponents/TouchReactionConditionComponent/TouchReactionConditionPR.cs
+++ Assets/Scripts/ProjectScripts/GameLogic/BoardComponents/TouchReactionConditionComponent/TouchReactionConditionPR.cs
@@ -1,10 +1,8 @@
-﻿using System.Collections.Generic;
-
-public class TouchReactionConditionPR : TouchReactionConditionStorage
+﻿public class TouchReactionConditionPR : TouchReactionConditionStorage
 {
     public override bool Check(BoardPosition position, Piece piece)
     {
-        var save = ProfileService.Current.GetComponent<TutorialSaveComponent>(TutorialSaveComponent.ComponentGuid)?.Complete ?? new List<int>();
+        var save = BoardService.Current.FirstBoard.TutorialLogic.Save;
 
         return save.Contains(8) && base.Check(position, piece);
     }
