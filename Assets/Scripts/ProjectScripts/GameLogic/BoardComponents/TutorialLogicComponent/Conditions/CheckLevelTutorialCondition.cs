c63d0bac79851dc0bdf1151b69b94ab7490f4c84
OlegDolmatovih <oleg@neskinsoft.com>
2018-11-19 00:35:42 +0300

    add firefly lock

---
+++ Assets/Scripts/ProjectScripts/GameLogic/BoardComponents/TutorialLogicComponent/Conditions/CheckLevelTutorialCondition.cs
@@ -0,0 +1,11 @@
+﻿public class CheckLevelTutorialCondition : BaseTutorialCondition
+{
+	public int Target;
+	
+	public override bool Check()
+	{
+		var level = GameDataService.Current?.LevelsManager?.Level;
+		
+		return level != null && level >= Target;
+	}
+}
