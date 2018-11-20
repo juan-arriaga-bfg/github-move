d5bfdbf90d3b1fcb694eae57c1ce7d65ca6c9847
George Alekhnovitch <>
2018-11-20 11:50:42 +0300

    up

--- Assets/Scripts/Unprocessed/IWTextMeshAnimation.cs
+++ Assets/Scripts/Unprocessed/IWTextMeshAnimation.cs
@@ -178,14 +178,17 @@ public class IWTextMeshAnimation : MonoBehaviour
         Color[] colors = mesh.colors;
         Color[] defaultColors = mesh.colors;
         targetColors = colors;
+        
+        yield return new WaitForEndOfFrame();
 
         int charCount = textLabel.textInfo.characterInfo.Length;//textLabel.textInfo.characterCount;
 
         // Why characterCount may != characterInfo.Length happens ?!
-//        while (textLabel.textInfo.characterCount != textLabel.textInfo.characterInfo.Length)
-//        {
-//            yield return null;
-//        }
+        while (textLabel.textInfo.characterCount != textLabel.textInfo.characterInfo.Length)
+        {
+            Debug.LogError("textLabel.textInfo.characterCount != textLabel.textInfo.characterInfo.Length");
+            yield return null;
+        }
         
         List<int> processedVertexIndex = new List<int>();
 
