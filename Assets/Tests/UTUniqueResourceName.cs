dfb73f87e6eab6e3983e5be4dfff7e69408c2d9e
George Alekhnovitch <>
2018-11-20 16:39:52 +0300

    fix FogExplosionParticleSystem prefab name

--- Assets/Tests/UTUniqueResourceName.cs
+++ Assets/Tests/UTUniqueResourceName.cs
@@ -2,13 +2,29 @@
 using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
+using UnityEditor;
 
 public class UTUniqueResourceName {
 
     [Test]
     public void UTUniqueResourceNameSimplePasses() {
         // Use the Assert class to test conditions.
-        
+        // var scriptsGuids = AssetDatabase.FindAssets("", new string[]{});
+        // var filteredScriptPath = new List<string>();
+        // var filteredScriptAbsolutePath = new List<string>();
+        // for (int i = 0; i < scriptsGuids.Length; i++)
+        // {
+        //     var scriptGuid = scriptsGuids[i];
+        //     var scriptPath = AssetDatabase.GUIDToAssetPath(scriptGuid);
+        //     if ((scriptPath.Contains($"{Path.DirectorySeparatorChar}Editor{Path.DirectorySeparatorChar}")) && scriptPath.Contains(".cs"))
+        //     {
+        //         filteredScriptPath.Add(scriptPath);
+        //         filteredScriptAbsolutePath.Add(Path.Combine(Application.dataPath, scriptPath.Replace($"Assets{Path.DirectorySeparatorChar}", "")));
+        //         
+        //     }
+        // }
+        //
+        // filteredScriptAbsolutePath.ForEach((path) => { Debug.LogWarning($"{path}");});
     }
 
     // A UnityTest behaves like a coroutine in PlayMode
