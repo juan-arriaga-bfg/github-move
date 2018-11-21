9e9cb41189afa0582daa1223bbf7098b940970b4
Alex Sorokin <alex@neskinsoft.com>
2018-11-21 13:21:30 +0300

    UT up

--- Assets/Tests/UTQuestIds.cs
+++ Assets/Tests/UTQuestIds.cs
@@ -5,120 +5,122 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 
-public class UTQuestIds 
+namespace UT
 {
-
-    [Test]
-    public void UTQuestIdsPasses()
+    public class UTQuestIds
     {
-        LogAssert.ignoreFailingMessages = true;
-        
-        QuestsDataManager questsDataManager = new QuestsDataManager();
-        questsDataManager.Reload();
-        if (!questsDataManager.CreateStarters())
+        [Test]
+        public void UTQuestIdsPasses()
         {
-            Assert.Fail("Can't create starters");
-            return;
-        }
-        
-        StringBuilder sb = new StringBuilder();
+            LogAssert.ignoreFailingMessages = true;
 
-        int startersCnt = 0;
-        int questsCnt = 0;
-        int tasksCnt = 0;
-        
-        var starters = questsDataManager.QuestStarters;
-        
-        foreach (var starter in starters)
-        {
-            startersCnt++;
-            
-            if (!ValidateId(starter.Id))
+            QuestsDataManager questsDataManager = new QuestsDataManager();
+            questsDataManager.Reload();
+            if (!questsDataManager.CreateStarters())
             {
-                sb.AppendLine($"Wrong starter id '{starter.Id}'"); 
+                Assert.Fail("Can't create starters");
+                return;
             }
-            
-            var questIds = starter.QuestToStartIds;
-            foreach (var questId in questIds)
-            {               
-                if (!ValidateId(questId))
-                {
-                    sb.AppendLine($"Wrong quest id '{questId}' in starter '{starter.Id}'"); 
-                    continue;
-                }
-                
-                var quest = questsDataManager.InstantiateQuest(questId);
-                if (quest == null)
-                {
-                    sb.AppendLine($"Starter '{starter.Id}' refers to not existed quest id '{questId}'"); 
-                    continue;
-                }
-                
-                questsCnt++;
 
-                foreach (var def in quest.TaskDefs)
+            StringBuilder sb = new StringBuilder();
+
+            int startersCnt = 0;
+            int questsCnt = 0;
+            int tasksCnt = 0;
+
+            var starters = questsDataManager.QuestStarters;
+
+            foreach (var starter in starters)
+            {
+                startersCnt++;
+
+                if (!ValidateId(starter.Id))
                 {
-                    if (!ValidateId(def.TaskId))
+                    sb.AppendLine($"Wrong starter id '{starter.Id}'");
+                }
+
+                var questIds = starter.QuestToStartIds;
+                foreach (var questId in questIds)
+                {
+                    if (!ValidateId(questId))
                     {
-                        sb.AppendLine($"Wrong task id '{def.TaskId}' for quest '{quest.Id}'"); 
-                    }
-                    
-                    var task = questsDataManager.InstantiateTask(def.TaskId);
-                    if (task == null)
-                    {
-                        sb.AppendLine($"Quest '{quest.Id}' refers to not existed task id '{def.TaskId}'"); 
+                        sb.AppendLine($"Wrong quest id '{questId}' in starter '{starter.Id}'");
+                        continue;
                     }
 
-                    tasksCnt++;
+                    var quest = questsDataManager.InstantiateQuest(questId);
+                    if (quest == null)
+                    {
+                        sb.AppendLine($"Starter '{starter.Id}' refers to not existed quest id '{questId}'");
+                        continue;
+                    }
+
+                    questsCnt++;
+
+                    foreach (var def in quest.TaskDefs)
+                    {
+                        if (!ValidateId(def.TaskId))
+                        {
+                            sb.AppendLine($"Wrong task id '{def.TaskId}' for quest '{quest.Id}'");
+                        }
+
+                        var task = questsDataManager.InstantiateTask(def.TaskId);
+                        if (task == null)
+                        {
+                            sb.AppendLine($"Quest '{quest.Id}' refers to not existed task id '{def.TaskId}'");
+                        }
+
+                        tasksCnt++;
+                    }
                 }
+            }
+
+            // Can't handle reuse, commented out
+            // int processedCount = startersCnt + questsCnt + tasksCnt;
+            // int cacheCount = questsDataManager.GetCountOfItemsInCache();
+            // if (processedCount != cacheCount)
+            // {
+            //     sb.AppendLine($"Only {processedCount} items used but {cacheCount} defined. Please ensure that all quests added to starters and all tasks added to quests."); 
+            // }
+
+            var message = sb.ToString();
+            if (string.IsNullOrEmpty(message))
+            {
+                Assert.Pass($"Starters: {startersCnt}, quests: {questsCnt}, tasks: {tasksCnt}");
+            }
+            else
+            {
+                Assert.Fail(message);
             }
         }
 
-        // Can't handle reuse, commented out
-        // int processedCount = startersCnt + questsCnt + tasksCnt;
-        // int cacheCount = questsDataManager.GetCountOfItemsInCache();
-        // if (processedCount != cacheCount)
-        // {
-        //     sb.AppendLine($"Only {processedCount} items used but {cacheCount} defined. Please ensure that all quests added to starters and all tasks added to quests."); 
+        // A UnityTest behaves like a coroutine in PlayMode
+        // and allows you to yield null to skip a frame in EditMode
+        // [UnityTest]
+        // public IEnumerator UTQuestIdsWithEnumeratorPasses() {
+        //     // Use the Assert class to test conditions.
+        //     // yield to skip a frame
+        //     yield return null;
         // }
 
-        var message = sb.ToString();
-        if (string.IsNullOrEmpty(message))
+        private bool ValidateId(string id)
         {
-            Assert.Pass($"Starters: {startersCnt}, quests: {questsCnt}, tasks: {tasksCnt}");
-        }
-        else
-        {
-            Assert.Fail(message);
-        }
-    }
-
-    // A UnityTest behaves like a coroutine in PlayMode
-    // and allows you to yield null to skip a frame in EditMode
-    // [UnityTest]
-    // public IEnumerator UTQuestIdsWithEnumeratorPasses() {
-    //     // Use the Assert class to test conditions.
-    //     // yield to skip a frame
-    //     yield return null;
-    // }
-
-    private bool ValidateId(string id)
-    {
-        if (string.IsNullOrEmpty(id))
-        {
-            return false;
-        }
-        
-        const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-_.";
-        
-        foreach (var c in id)
-        {
-            if (!alphabet.Contains(c))
+            if (string.IsNullOrEmpty(id))
             {
                 return false;
             }
+
+            const string ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-_.";
+
+            foreach (var c in id)
+            {
+                if (!ALPHABET.Contains(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
-        
-        return true;
     }
 }
