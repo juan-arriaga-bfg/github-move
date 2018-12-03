7d564f3875fb8d543219a144dce17bdfa690e0ee
Alex Sorokin <alex@neskinsoft.com>
2018-12-03 20:34:30 +0300

    Tasks highlight up

---
+++ Assets/Scripts/ProjectScripts/BusinessLogic/GameData/Quests/Highlight/HighlightTaskCollectFirefly.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class HighlightTaskCollectFirefly : ITaskHighlight
+{
+    public const float DELAY_BEFORE_SHOW_ARROW = 0.5f;
+    
+    public bool Highlight(TaskEntity task)
+    {
+        return HighlightVisibleFirefly();
+    }
+
+    private bool HighlightVisibleFirefly()
+    {
+        var boardLogic = BoardService.Current.FirstBoard.BoardLogic;
+        var fireflyLogic = boardLogic.FireflyLogic;
+        var fireflyViews = fireflyLogic.Views;
+
+        if (fireflyViews == null || fireflyViews.Count == 0)
+        {
+            return false;
+        }
+        
+        var camera = Camera.main;
+        if (camera == null)
+        {
+            return false;
+        }
+        
+        var cameraManipulator = camera.GetComponent<CameraManipulator>();
+        var region = cameraManipulator.CurrentCameraSettings.CameraClampRegion;
+
+        var visibleFireflies = new List<FireflyView>();
+        
+        foreach (var view in fireflyViews)
+        {
+            var pos = view.transform.position;
+            if (region.Contains(pos))
+            {
+                visibleFireflies.Add(view);
+            }
+        }
+
+        if (visibleFireflies.Count == 0)
+        {
+            return false;
+        }
+
+        int index = Random.Range(0, visibleFireflies.Count);
+        var selectedView = visibleFireflies[index];
+        
+        var board = BoardService.Current.FirstBoard;
+        board.Manipulator.CameraManipulator.MoveTo(selectedView.transform.position);
+        
+        DOTween.Sequence()
+               .AppendInterval(DELAY_BEFORE_SHOW_ARROW)
+               .AppendCallback(() =>
+                {
+                    if (selectedView != null)
+                    {
+                        selectedView.AddArrow();
+                    }
+                });
+        
+        return true;
+    }
+}
