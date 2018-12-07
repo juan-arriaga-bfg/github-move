8e55f1ce8771c5a64c4407af1246deeaa53e6597
Alex Sorokin <alex@neskinsoft.com>
2018-12-07 12:44:25 +0300

    Daily up

--- Assets/Scripts/ProjectScripts/BusinessLogic/GameData/Quests/Quest/DailyQuestEntity.cs
+++ Assets/Scripts/ProjectScripts/BusinessLogic/GameData/Quests/Quest/DailyQuestEntity.cs
@@ -8,10 +8,10 @@ public class DailyQuestEntity : QuestEntity
 {   
     private readonly Dictionary<TaskGroup, int> tasksCount = new Dictionary<TaskGroup, int>
     {
-        {TaskGroup.Permanent, 20},
-        {TaskGroup.Easy,      20},
-        {TaskGroup.Normal,    20},
-        {TaskGroup.Hard,      10},
+        {TaskGroup.Permanent, 2},
+        {TaskGroup.Easy,      2},
+        {TaskGroup.Normal,    2},
+        {TaskGroup.Hard,      1},
     };
 
     // Set this to prevent QuestManager from replacing this instance on timer completion
@@ -66,7 +66,7 @@ public class DailyQuestEntity : QuestEntity
             int count = def.Value;
 
             List<TaskEntity> tasksPool = tasksDict[group];
-            if (tasksPool.Count > tasksPool.Count)
+            if (count > tasksPool.Count)
             {
                 Debug.LogError($"[DailyQuestEntity] => Not enough tasks in pool to fill '{def.Key}' group. At least {count} tasks expected.");
                 continue;
