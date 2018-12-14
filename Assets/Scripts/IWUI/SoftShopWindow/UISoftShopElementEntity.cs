b2b59db37b886af7d78ad47226bf355c15ceba07
OlegDolmatovih <oleg@neskinsoft.com>
2018-12-14 15:27:01 +0300

    fix "+"

--- Assets/Scripts/IWUI/SoftShopWindow/UISoftShopElementEntity.cs
+++ Assets/Scripts/IWUI/SoftShopWindow/UISoftShopElementEntity.cs
@@ -1,6 +1,6 @@
 ﻿public class UISoftShopElementEntity : UISimpleScrollElementEntity
 {
-    public override string LabelText => $"+{Product.ToStringIcon()}";
+    public override string LabelText => $"<size=35><sprite name={Product.Currency}></size>{Product.Amount}";
     public string ButtonLabel => string.Format(LocalizationService.Get("common.button.buy", "common.button.buy {0}"), Price.ToStringIcon());
     
     public CurrencyPair Product;
