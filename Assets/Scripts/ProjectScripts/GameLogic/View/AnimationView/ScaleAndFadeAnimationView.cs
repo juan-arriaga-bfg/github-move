ec9e5bd72ed4e85eec216d04e84158fb3c12d51e
DanilaZhelenok <daniel@neskinsoft.com>
2018-12-26 17:15:57 +0300

    return scale animation on destroy NPC_Gnome

---
+++ Assets/Scripts/ProjectScripts/GameLogic/View/AnimationView/ScaleAndFadeAnimationView.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class ScaleAndFadeAnimationView : AnimationView
+{
+    [SerializeField] private float toFade = 0;
+    [SerializeField] private Vector3 toScale = new Vector3(1, 1, 1);
+    [SerializeField] private float scaleDuration = 0.2f;
+    [SerializeField] private float fadeDuration = 0.2f;
+    [SerializeField] private float timeoutDuration = 0.5f;
+    [SerializeField] private Ease easeType = Ease.Linear;
+    
+    private List<SpriteRenderer> spriteRenderers = new List<SpriteRenderer>();
+    protected PieceBoardElementView pieceView = null;
+    public override void Play(PieceBoardElementView pieceView)
+    {
+        base.Play(pieceView);
+
+        var sequence = DOTween.Sequence();
+        sequence.SetId(animationUid);
+
+        spriteRenderers = pieceView.GetCachedSpriteRenderers();
+        foreach (var spriteRenderer in spriteRenderers)
+        {
+            sequence.Insert(0, spriteRenderer.DOFade(toFade, fadeDuration).SetEase(easeType));
+        }
+
+        sequence.Insert(0, pieceView.transform.DOScale(toScale, scaleDuration).SetEase(easeType));
+       
+        sequence.InsertCallback(timeoutDuration, () => OnComplete?.Invoke());
+        
+        this.pieceView = pieceView;
+    }
+
+    public override void Stop()
+    {
+        if (this.pieceView != null)
+            DOTween.Kill(animationUid);
+    }
+}
