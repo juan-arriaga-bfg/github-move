dc8c6fe2de00e1a4eea66eae0265ae33a919fe35
Alex Sorokin <alex@neskinsoft.com>
2019-01-03 21:45:53 +0300

    Loading fix

--- Assets/Scripts/Unprocessed/AsyncInit/MainSceneLoaderComponent.cs
+++ Assets/Scripts/Unprocessed/AsyncInit/MainSceneLoaderComponent.cs
@@ -1,24 +1,27 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
 
 public class MainSceneLoaderComponent : AsyncInitComponent
 {
     private AsyncOperation loadingOperation;
     
-    public override bool IsCompleted => true;
+    public override bool IsCompleted => loadingOperation != null && loadingOperation.isDone;
 
     public override float Progress
     {
         get
         {
-            if (IsCompleted)
-            {
-                return 1;
-            }
-
             if (loadingOperation == null)
             {
                 return 0;
+            }
+            
+            if (loadingOperation.isDone)
+            {
+                return 1;
             }
 
             return loadingOperation.progress;
@@ -27,6 +30,24 @@ public class MainSceneLoaderComponent : AsyncInitComponent
 
     public override void OnRegisterEntity(ECSEntity entity)
     {
+        // Proxy GO to run coroutine from non-monobehaviour
+        var go = new GameObject();
+        var mb = go.AddComponent<IWBaseMonoBehaviour>();
+        Object.DontDestroyOnLoad(go);
+        
+        mb. StartCoroutine(LoadSceneCoroutine(() => Object.Destroy(go)));
+    }
+    
+    IEnumerator LoadSceneCoroutine(Action onComplete)
+    {
         loadingOperation = SceneManager.LoadSceneAsync("Main", LoadSceneMode.Single);
+
+        // Wait until the asynchronous scene fully loads
+        while (!loadingOperation.isDone)
+        {
+            yield return null;
+        }
+
+        onComplete();
     }
 }
