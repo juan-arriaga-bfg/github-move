cd4350d09b8d237cd70c3c0836027fe14be8c5e9
OlegDolmatovih <oleg@neskinsoft.com>
2019-01-18 20:04:50 +0300

    up hard shop

--- Assets/Scripts/ProjectScripts/BusinessLogic/GameData/Shop/ShopDef.cs
+++ Assets/Scripts/ProjectScripts/BusinessLogic/GameData/Shop/ShopDef.cs
@@ -5,6 +5,7 @@ public class ShopDef
     public string Uid;
     public string Icon;
     public string Name;
+    public string PurchaseKay;
     public List<CurrencyPair> Products;
     public CurrencyPair Price;
 }
