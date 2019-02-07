237e996fb7656adf17168ddee89a679845f36f1f
DanilaZhelenok <daniel@neskinsoft.com>
2019-02-07 18:54:16 +0300

    fix AnimationOverrideDataManager

--- Assets/Scripts/ProjectScripts/GameLogic/BoardComponents/TutorialLogicComponent/Animations/RemoveTutorialAnimation.cs
+++ Assets/Scripts/ProjectScripts/GameLogic/BoardComponents/TutorialLogicComponent/Animations/RemoveTutorialAnimation.cs
@@ -41,6 +41,6 @@ public class RemoveTutorialAnimation : BaseTutorialAnimation
         
         DOTween.Sequence()
             .AppendInterval(0.5f)
-            .AppendCallback(() => board.ActionExecutor.AddAction(new CollapsePieceToAction{To = target, Positions = new List<BoardPosition>{target}, AnimationResourceSearch = piece => AnimationDataManager.FindAnimation(piece, def => def.OnDestroyFromBoard)}));
+            .AppendCallback(() => board.ActionExecutor.AddAction(new CollapsePieceToAction{To = target, Positions = new List<BoardPosition>{target}, AnimationResourceSearch = piece => AnimationOverrideDataService.Current.FindAnimation(piece, def => def.OnDestroyFromBoard)}));
     }
 }
