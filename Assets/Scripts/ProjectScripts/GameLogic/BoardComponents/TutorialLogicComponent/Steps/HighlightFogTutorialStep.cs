b4827a3d013b99968250c31332bc7e8168076b4d
OlegDolmatovih <oleg@neskinsoft.com>
2019-02-07 21:14:05 +0300

    up fog tutorial

--- Assets/Scripts/ProjectScripts/GameLogic/BoardComponents/TutorialLogicComponent/Steps/HighlightFogTutorialStep.cs
+++ Assets/Scripts/ProjectScripts/GameLogic/BoardComponents/TutorialLogicComponent/Steps/HighlightFogTutorialStep.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.Generic;
+using DG.Tweening;
 
 public class HighlightFogTutorialStep : LoopFingerTutorialStep
 {
     private readonly int mana = PieceType.Mana1.Id;
     private bool isSpawn;
+    
+    private BubbleView bubble;
     
     public override void Perform()
     {
@@ -36,16 +39,42 @@ public class HighlightFogTutorialStep : LoopFingerTutorialStep
     {
         var positions = Context.Context.BoardLogic.PositionsCache.GetPiecePositionsByType(mana);
 
-        if (positions.Count == 0) return;
+        if (positions.Count == 0)
+        {
+            StepBubble();
+            return;
+        }
         
         from = positions[0];
         to = GameDataService.Current.FogsManager.GetNextRandomFog().GetCenter();
         
         base.Execute();
     }
+    
+    private void StepBubble()
+    {
+        var position = to;
+        position.Z = BoardLayer.Piece.Layer;
+        
+        var piece = Context.Context.BoardLogic.GetPieceAt(position);
+        
+        bubble = piece?.ViewDefinition?.GetViews().Find(view => view is BubbleView) as BubbleView;
+
+        if (bubble == null) return;
+        
+        var sequence = DOTween.Sequence().SetId(this).SetLoops(int.MaxValue);
+
+        sequence.AppendCallback(bubble.Attention);
+        sequence.AppendInterval(1f);
+        sequence.AppendCallback(bubble.Attention);
+        sequence.AppendInterval(2f);
+    }
 
     protected override void Complete()
     {
+        DOTween.Kill(this);
+        bubble = null;
+        
         base.Complete();
         
         Context.Context.HintCooldown.Resume(this);
@@ -53,6 +82,6 @@ public class HighlightFogTutorialStep : LoopFingerTutorialStep
 
     public override bool IsExecuteable()
     {
-        return isSpawn && base.IsExecuteable();
+        return isSpawn && bubble == null && base.IsExecuteable();
     }
 }
