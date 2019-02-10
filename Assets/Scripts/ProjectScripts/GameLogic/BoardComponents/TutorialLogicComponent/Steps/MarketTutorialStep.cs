d8e92080bbb50870f780a9b3a9059aa78cf585a8
OlegDolmatovih <oleg@neskinsoft.com>
2019-02-10 16:23:15 +0300

    fix tutorial arrow

--- Assets/Scripts/ProjectScripts/GameLogic/BoardComponents/TutorialLogicComponent/Steps/MarketTutorialStep.cs
+++ Assets/Scripts/ProjectScripts/GameLogic/BoardComponents/TutorialLogicComponent/Steps/MarketTutorialStep.cs
@@ -1,17 +1,32 @@
 ﻿public class MarketTutorialStep : BaseTutorialStep
 {
+    private UIHintArrowViewController arrow;
+    
+    public override void PauseOn()
+    {
+        base.PauseOn();
+        
+        if(arrow == null) return;
+        
+        arrow.Hide(false);
+    }
+
+    public override void PauseOff()
+    {
+        base.PauseOff();
+        
+        if(arrow == null) return;
+        
+        CreateArrow();
+    }
+    
     public override void Perform()
     {
         if(IsPerform) return;
         
         base.Perform();
         
-        var model = UIService.Get.GetCachedModel<UIMarketWindowModel>(UIWindowType.MarketWindow);
-        var view = UIService.Get.GetShowedView<UIMainWindowView>(UIWindowType.MainWindow);
-
-        model.IsTutorial = true;
-
-        view.CachedHintArrowComponent.ShowArrow(view.HintAnchorShopButton, -1);
+        CreateArrow();
     }
     
     protected override void Complete()
@@ -24,5 +39,17 @@
         model.IsTutorial = false;
 
         view.CachedHintArrowComponent.HideArrow(view.HintAnchorShopButton);
+
+        arrow = null;
+    }
+
+    private void CreateArrow()
+    {
+        var model = UIService.Get.GetCachedModel<UIMarketWindowModel>(UIWindowType.MarketWindow);
+        var view = UIService.Get.GetShowedView<UIMainWindowView>(UIWindowType.MainWindow);
+
+        model.IsTutorial = true;
+
+        arrow = view.CachedHintArrowComponent.ShowArrow(view.HintAnchorShopButton, -1);
     }
 }
