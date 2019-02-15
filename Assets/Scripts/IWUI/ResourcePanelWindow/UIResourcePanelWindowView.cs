c662d0bee41cb0ea1c1f5f761a948b596d7aed5e
Alex Sorokin <alex@neskinsoft.com>
2019-02-15 11:53:42 +0300

    Revert "Revert "Revert "Fix re#1948 - если быстро скликивать подарки в кодексе - ломается полет монет"""
    This reverts commit c0ec739e9765cbe3daae62b7f1f527b29544e100.

--- Assets/Scripts/IWUI/ResourcePanelWindow/UIResourcePanelWindowView.cs
+++ Assets/Scripts/IWUI/ResourcePanelWindow/UIResourcePanelWindowView.cs
@@ -52,8 +52,6 @@ public class UIResourcePanelWindowView : UIBaseWindowView
     
     private readonly Dictionary<string, CanvasGroup> cachedResourcePanelContainerDown = new Dictionary<string, CanvasGroup>();
     
-    private readonly Dictionary<object, LockerComponent> lockers = new Dictionary<object, LockerComponent>();
-    
     public UIHintArrowComponent CachedHintArrowComponent { get; private set; }
     
     public override void InitView(IWWindowModel model, IWWindowController controller)
@@ -84,31 +82,27 @@ public class UIResourcePanelWindowView : UIBaseWindowView
         }
     }
     
-    public void ToggleFadePanel(string currency, bool state)
+    public virtual void ToggleFadePanel(string currency, bool state)
     {
         Transform targetPanel;
         CanvasGroup targetContainer;
-
-        if (Toggle(currency, state))
+        if (state)
         {
-            if (state)
+            if (cachedResourcePanels.TryGetValue(currency, out targetPanel) && cachedResourcePanelContainerUp.TryGetValue(currency, out targetContainer))
             {
-                if (cachedResourcePanels.TryGetValue(currency, out targetPanel) && cachedResourcePanelContainerUp.TryGetValue(currency, out targetContainer))
-                {
-                    targetPanel.SetParent(targetContainer.transform);
-                }
+                targetPanel.SetParent(targetContainer.transform);
             }
-            else
+        }
+        else
+        {
+            if (cachedResourcePanels.TryGetValue(currency, out targetPanel) && cachedResourcePanelContainerDown.TryGetValue(currency, out targetContainer))
             {
-                if (cachedResourcePanels.TryGetValue(currency, out targetPanel) && cachedResourcePanelContainerDown.TryGetValue(currency, out targetContainer))
-                {
-                    targetPanel.SetParent(targetContainer.transform);
-                }
+                targetPanel.SetParent(targetContainer.transform);
             }
         }
     }
 
-    public void TogglePanel(string currency, bool state, bool isAnimate = false)
+    public virtual void TogglePanel(string currency, bool state, bool isAnimate = false)
     {
         var alpha = state ? 1f : 0f;
 
@@ -118,66 +112,36 @@ public class UIResourcePanelWindowView : UIBaseWindowView
         {
             return;
         }
-
-        if (Toggle(currency, state))
+        
+        if (isAnimate)
         {
-            if (isAnimate)
+            DOTween.Kill(targetContainerUp);
+            targetContainerUp.DOFade(alpha, 0.35f).SetId(targetContainerUp);
+
+            DOTween.Kill(targetContainerDown);
+            targetContainerDown.DOFade(alpha, 0.35f).SetId(targetContainerDown);
+
+            DOTween.Kill(targetPanel);
+            
+            if (state == false)
             {
-                DOTween.Kill(targetContainerUp);
-                targetContainerUp.DOFade(alpha, 0.35f).SetId(targetContainerUp);
-
-                DOTween.Kill(targetContainerDown);
-                targetContainerDown.DOFade(alpha, 0.35f).SetId(targetContainerDown);
-
-                DOTween.Kill(targetPanel);
-
-                if (state == false)
-                {
-                    DOTween.Sequence().SetId(targetPanel).AppendInterval(0.35f).OnComplete(() => { targetPanel.gameObject.SetActive(false); });
-                }
-                else
-                {
-                    targetPanel.gameObject.SetActive(true);
-                }
+                DOTween.Sequence().SetId(targetPanel).AppendInterval(0.35f).OnComplete(() => { targetPanel.gameObject.SetActive(false); });
             }
             else
             {
-                DOTween.Kill(targetContainerUp);
-                DOTween.Kill(targetContainerDown);
-                DOTween.Kill(targetPanel);
-
-                targetContainerUp.alpha = alpha;
-                targetContainerDown.alpha = alpha;
-                targetPanel.gameObject.SetActive(state);
+                targetPanel.gameObject.SetActive(true);
             }
-        }
-    }
-
-    private bool Toggle(object item, bool state)
-    {
-        bool isNewLocker = false;
-        
-        if (!lockers.TryGetValue(item, out LockerComponent locker))
-        {
-            locker = new LockerComponent();
-            lockers.Add(item, locker);
-            isNewLocker = true;
-        }
-
-        bool isLockedBefore = locker.IsLocked;
-        
-        if (state)
-        {
-            locker.Lock(item, true);
         }
         else
         {
-            locker.Unlock(item);
+            DOTween.Kill(targetContainerUp);
+            DOTween.Kill(targetContainerDown);
+            DOTween.Kill(targetPanel);
+                
+            targetContainerUp.alpha = alpha;
+            targetContainerDown.alpha = alpha;
+            targetPanel.gameObject.SetActive(state);
         }
-
-        var ret = isNewLocker || isLockedBefore != locker.IsLocked;
-        
-        return ret;
     }
 
     public virtual void TogglePanels(bool state, bool isAnimate = false, List<string> ignorePanels = null)
