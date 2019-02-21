c0400a3c74f2d2242841b4e45f056f3b0d108fc5
Alex Sorokin <alex@neskinsoft.com>
2019-02-21 11:44:39 +0300

    EcsExtentions add

---
+++ Assets/Scripts/Extensions/ECSExtentions.cs
@@ -0,0 +1,36 @@
+using System.Linq;
+using UnityEngine;
+
+public static class ECSExtentions
+{
+    public static void UnregisterRecursive(this IECSComponent itemToCleanup, ECSEntity parent = null)
+    {
+        // Debug.Log("======== UnregisterRecursive: " + itemToCleanup.GetType());
+        switch (itemToCleanup)
+        {
+            case ECSEntity entity:
+                var ecsComponents = entity.ComponentsCache.Values.ToList();
+                for (var i = ecsComponents.Count - 1; i >= 0; i--)
+                {
+                    var item = ecsComponents[i];
+                    item.UnregisterRecursive(entity);
+                }
+
+                break;
+            
+            case ECSComponentCollection collection:
+                foreach (var item in collection.Components)
+                {
+                    item.UnregisterRecursive();
+                }
+                break;
+            
+            case IECSSystem system:
+                ECSService.Current.SystemProcessor.UnRegisterSystem(system);
+                break;
+        }
+        
+        // Debug.Log($"== UnregisterRecursive: {itemToCleanup.GetType()} with context: {context?.GetType()}");
+        parent?.UnRegisterComponent(itemToCleanup);
+    }
+}
