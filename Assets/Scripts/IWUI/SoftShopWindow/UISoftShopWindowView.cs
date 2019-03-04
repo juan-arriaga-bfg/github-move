57b4d4174708d5b1a5627794d6675703efa9a93a
OlegDolmatovih <oleg@neskinsoft.com>
2019-03-04 15:48:18 +0300

    up soft shop

--- Assets/Scripts/IWUI/SoftShopWindow/UISoftShopWindowView.cs
+++ Assets/Scripts/IWUI/SoftShopWindow/UISoftShopWindowView.cs
@@ -1,3 +1,11 @@
 public class UISoftShopWindowView : UIShopWindowView
 {
+    public override void OnViewShow()
+    {
+        base.OnViewShow();
+        
+        var windowModel = Model as UIShopWindowModel;
+
+        content.GetScrollRect().enabled = windowModel.Products.Count > 3;
+    }
 }
