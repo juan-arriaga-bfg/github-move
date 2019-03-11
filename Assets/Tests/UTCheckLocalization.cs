ef7ba858bed914f773527dbc3dfac014f5de900f
George Alekhnovitch <>
2019-03-11 18:11:01 +0300

    up localization fixer

--- Assets/Tests/UTCheckLocalization.cs
+++ Assets/Tests/UTCheckLocalization.cs
@@ -1,7 +1,7 @@
 ﻿using System.Text.RegularExpressions;
 using NUnit.Framework;
 using UnityEngine;
-using Assert = UnityEngine.Assertions.Assert;
+using System.Linq;
 
 namespace UT
 {
@@ -31,8 +31,15 @@ namespace UT
                 
                 Debug.LogWarning($"[Check Localization] => Localization {langName} checked status:{isValid}");
             }
-            
-            Assert.IsTrue(state);
+
+            if (state)
+            {
+                Assert.Pass();
+            }
+            else
+            {
+                Assert.Fail();
+            }
         }
         
         protected bool CheckLocalizationData(LocalizationManager localizationManager, string currentLocalization)
@@ -41,7 +48,7 @@ namespace UT
             
             foreach (var localizationData in localizationManager.StorageItems)
             {
-                bool isValidData = IsLocalizationDataValid(localizationData, currentLocalization);
+                bool isValidData = LocalizationEditorUtils.IsLocalizationDataValid(localizationData, currentLocalization);
 
                 if (isValidData == false)
                 {
@@ -52,69 +59,7 @@ namespace UT
             return state;
         }
 
-        protected bool IsLocalizationDataValid(LocalizationData localizationData, string currentLocalization)
-        {
-            bool state = true;
-
-            if (string.IsNullOrEmpty(localizationData.Uid))
-            {
-                state = false;
-                Debug.LogWarning($"[Check Localization] => empty KEY for:{localizationData.Uid} lang:{currentLocalization}");
-            }
-            else if (string.IsNullOrEmpty(localizationData.Text))
-            {
-                state = false;
-                Debug.LogWarning($"[Check Localization] => empty VALUE for:{localizationData.Uid} lang:{currentLocalization}");
-            }
-            else if (localizationData.Uid.Contains("\""))
-            {
-                state = false;
-                Debug.LogWarning($"[Check Localization] => KEY has symbol '\' for:{localizationData.Uid} lang:{currentLocalization}");
-            }
-            else if (localizationData.Uid.Contains("&"))
-            {
-                // replace to &amp;
-                state = false;
-                Debug.LogWarning($"[Check Localization] => KEY has symbol '\' for:{localizationData.Uid} lang:{currentLocalization}");
-            }
-            else if (localizationData.Text.Contains("\""))
-            {
-                // replace to &quot;
-                Debug.LogWarning($"[Check Localization] => VALUE has symbol '\' for:{localizationData.Uid} lang:{currentLocalization}");
-            }
-            else if (localizationData.Uid.Contains(" "))
-            {
-                // replace to (remove)
-                Debug.LogWarning($"[Check Localization] => KEY has symbol 'space' for:{localizationData.Uid} lang:{currentLocalization}");
-            }
-
-            if (currentLocalization.Equals(SystemLanguage.Russian.ToString()) == false)
-            {
-                Regex wrongSymbols = new Regex(@"S""[\p{IsCyrillic}\p{P}\p{N}\s]*""");
-                
-                if (wrongSymbols.IsMatch(localizationData.Uid))
-                {
-                    Debug.LogWarning($"[Check Localization] => KEY has wrong symbol [{wrongSymbols}] for:{localizationData.Uid} lang:{currentLocalization}");
-                }
-                else if (wrongSymbols.IsMatch(localizationData.Text))
-                {
-                    Debug.LogWarning($"[Check Localization] => KEY has wrong symbol [{wrongSymbols}] for:{localizationData.Uid} lang:{currentLocalization}");
-                }
-                else if (localizationData.Text.ToLower().Contains("с"))
-                {
-                    // replace to C,c (english);
-                    Debug.LogWarning($"[Check Localization] => VALUE has symbol 'c or C (ascii)' for:{localizationData.Uid} lang:{currentLocalization}");
-                }
-                else if (localizationData.Text.ToLower().Contains("х"))
-                {
-                    // replace to x,X (english);
-                    Debug.LogWarning($"[Check Localization] => VALUE has symbol 'х or Х (ascii)' for:{localizationData.Uid} lang:{currentLocalization}");
-                }
-            }
-            
-            
-            return state;
-        }
+        
 
     }
 
