b7f75a505022744a165b3f73c82b1cde16347c1f
OlegDolmatovih <oleg@neskinsoft.com>
2019-03-11 14:44:50 +0300

    up fog lock

--- Assets/Scripts/ProjectScripts/GameLogic/View/LockView/LockView.cs
+++ Assets/Scripts/ProjectScripts/GameLogic/View/LockView/LockView.cs
@@ -14,6 +14,9 @@ public class LockView : BoardElementView
     [SerializeField] private Material grayscaleMaterial;
     [SerializeField] private Transform hintArrowTarget;
     
+    [SerializeField] private GameObject levelCheck;
+    [SerializeField] private GameObject heroCheck;
+    
     private Material defaultMaterial;
 
     public Transform GetHintTarget()
@@ -27,20 +30,18 @@ public class LockView : BoardElementView
         defaultMaterial = levelLock.material;
     }
 
-    public void SetCondition(string level, string hero)
+    public void SetCondition(string level, string hero, bool isLevelUnlock, bool isHeroUnlock, bool isTwo)
     {
-        var isLevelUnlock = string.IsNullOrEmpty(level) == false;
-        var isHeroUnlock = string.IsNullOrEmpty(hero) == false;
-        var isTwo = isLevelUnlock && isHeroUnlock;
+        if (string.IsNullOrEmpty(level) == false) label.Text = level;
+        if (string.IsNullOrEmpty(hero) == false) icon.sprite = IconService.Current.GetSpriteById($"{hero}_Icon");
         
-        if(isLevelUnlock) label.Text = level;
-        if(isHeroUnlock) icon.sprite = IconService.Current.GetSpriteById($"{hero}_Icon");
+        heroLock.gameObject.SetActive(isTwo);
         
-        levelLock.gameObject.SetActive(isLevelUnlock);
-        heroLock.gameObject.SetActive(isHeroUnlock);
+        levelCheck.SetActive(isLevelUnlock);
+        heroCheck.SetActive(isHeroUnlock);
         
         levelLock.transform.localPosition = isTwo ? Vector3.right * 0.55f : Vector3.zero;
-        heroLock.transform.localPosition = isTwo ? Vector3.left * 0.55f : Vector3.zero;
+        heroLock.transform.localPosition = Vector3.left * 0.55f;
     }
     
     public void SetGrayscale(bool enabled)
