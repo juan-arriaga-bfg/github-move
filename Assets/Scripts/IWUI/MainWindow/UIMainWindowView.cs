5c1765fc9c035362cc87698b9ad087bca1e6a588
George Alekhnovitch <>
2019-03-12 18:42:51 +0300

    Revert "test spritesheets rebuild icons"
    This reverts commit 2581646cc884f1ac319b51de63b4a4fdbfc67159.

--- Assets/Scripts/IWUI/MainWindow/UIMainWindowView.cs
+++ Assets/Scripts/IWUI/MainWindow/UIMainWindowView.cs
@@ -26,8 +26,6 @@ public class UIMainWindowView : UIBaseWindowView
     [IWUIBinding("#QuestListDelimiters")] private GameObject questListDelimiters;
     
     [IWUIBinding("#DebugButtonsAnchor")] private Transform debugButtonsAnchor;
-    
-    [IWUIBinding("#TestIcon")] private Image testIcon;
 
     [Header("Hint anchors")] 
     [SerializeField] private Transform hintAnchorOrdersButton;
@@ -68,8 +66,6 @@ public class UIMainWindowView : UIBaseWindowView
         OnActiveQuestsListChanged();
         
         UpdateCodexButton();
-
-        testIcon.sprite = IconService.Current.GetSpriteById("test_icon");
     }
 
     public override void OnViewClose()
