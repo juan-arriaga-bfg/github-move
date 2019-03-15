527bfdf173c43fa80e719f77f97bda5fe00a43d2
Alex Sorokin <alex@neskinsoft.com>
2019-03-15 19:09:11 +0300

    Enable logger build action

--- Assets/Scripts/Editor/Build/Actions/BuildActionDisableLog.cs
+++ Assets/Scripts/Editor/Build/Actions/BuildActionDisableLog.cs
@@ -10,44 +10,44 @@ public class BuildActionDisableLogger : IProjectBuildAction
 {    
     public void Execute(ProjectBuilder context)
     {
-        // // Lets collect all defines
-        // HashSet<string> resultDefines = new HashSet<string>();
-        //
-        //
-        // BuildTargetGroup buildTarget;
-        // switch (context.CurrentBuildPlatform)
-        // {
-        //     case ProjectBuilder.BuildPlatform.Android:
-        //         buildTarget = BuildTargetGroup.Android;
-        //         break;
-        //     
-        //     case ProjectBuilder.BuildPlatform.Ios:
-        //         buildTarget = BuildTargetGroup.iOS;
-        //         break;
-        //     
-        //     default:
-        //         throw new ArgumentOutOfRangeException();
-        // }
-        //
-        // // Defines from platform settings
-        // List<string> existingDefines = GetDefinesFromPlatformSettings(buildTarget);
-        // foreach (var define in existingDefines)
-        // {
-        //     if (define == IW.Logger.ENABLE_DEFINE)
-        //     {
-        //         continue;
-        //     }
-        //     
-        //     resultDefines.Add(define); 
-        // }
-        //
-        // string resultDefinesStr = "";
-        // foreach (var define in resultDefines)
-        // {
-        //     resultDefinesStr = resultDefinesStr + define + ";";
-        // }
-        //
-        // PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTarget, resultDefinesStr);
+        // Lets collect all defines
+        HashSet<string> resultDefines = new HashSet<string>();
+        
+        
+        BuildTargetGroup buildTarget;
+        switch (context.CurrentBuildPlatform)
+        {
+            case ProjectBuilder.BuildPlatform.Android:
+                buildTarget = BuildTargetGroup.Android;
+                break;
+            
+            case ProjectBuilder.BuildPlatform.Ios:
+                buildTarget = BuildTargetGroup.iOS;
+                break;
+            
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+        
+        // Defines from platform settings
+        List<string> existingDefines = GetDefinesFromPlatformSettings(buildTarget);
+        foreach (var define in existingDefines)
+        {
+            if (define == Logger.ENABLE_DEFINE)
+            {
+                continue;
+            }
+            
+            resultDefines.Add(define); 
+        }
+        
+        string resultDefinesStr = "";
+        foreach (var define in resultDefines)
+        {
+            resultDefinesStr = resultDefinesStr + define + ";";
+        }
+        
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTarget, resultDefinesStr);
     }
     
     private static List<string> GetDefinesFromPlatformSettings(BuildTargetGroup targetGroup)
