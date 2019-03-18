f45923d1561479a5ea3da285054098c89e7abea1
OlegDolmatovih <oleg@neskinsoft.com>
2019-03-18 12:06:51 +0300

    fix tutorial starter

--- Assets/Scripts/ProjectScripts/GameLogic/BoardComponents/TutorialLogicComponent/Conditions/CheckQuestTutorialCondition.cs
+++ Assets/Scripts/ProjectScripts/GameLogic/BoardComponents/TutorialLogicComponent/Conditions/CheckQuestTutorialCondition.cs
@@ -7,8 +7,9 @@ public class CheckQuestTutorialCondition : BaseTutorialCondition
 	
 	public override bool Check()
 	{
-		var quest = GameDataService.Current?.QuestsManager?.ActiveQuests?.Find(entity => entity.Id == Target);
+		var finishedQuest = GameDataService.Current?.QuestsManager?.FinishedQuests?.Find(id => id == Target);
+		var activeQuest = GameDataService.Current?.QuestsManager?.ActiveQuests?.Find(entity => entity.Id == Target);
 		
-		return quest != null && quest.State >= TargetState;
+		return finishedQuest != null || activeQuest != null && activeQuest.State >= TargetState;
 	}
 }
