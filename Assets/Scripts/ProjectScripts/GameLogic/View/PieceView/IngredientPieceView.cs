5a235af02456b4f1edf59037acfbbca846cb5c03
DanilaZhelenok <daniel@neskinsoft.com>
2019-03-27 18:31:34 +0300

    add highlight for HardCurrency and SoftCurrency pieces

--- Assets/Scripts/ProjectScripts/GameLogic/View/PieceView/IngredientPieceView.cs
+++ Assets/Scripts/ProjectScripts/GameLogic/View/PieceView/IngredientPieceView.cs
@@ -1,64 +1,7 @@
 using DG.Tweening;
 using UnityEngine;
 
-public class IngredientPieceView : PieceBoardElementView
+public class IngredientPieceView : HighlightedPieceView
 {
-    [SerializeField] private Transform body;
-
-    public override void Init(BoardRenderer context, Piece piece)
-    {
-        base.Init(context, piece);
-
-        StartAnimation();
-    }
-
-    public override void OnDragStart(BoardPosition boardPos, Vector2 worldPos)
-    {
-        DOTween.Kill(body);
-        body.localScale = Vector3.one;
-        base.OnDragStart(boardPos, worldPos);
-    }
-
-    public override void OnDragEnd(BoardPosition boardPos, Vector2 worldPos)
-    {
-        base.OnDragEnd(boardPos, worldPos);
-        StartAnimation();
-    }
-
-    public override void ResetViewOnDestroy()
-    {
-        DOTween.Kill(body);
-        body.localScale = Vector3.one;
-        base.ResetViewOnDestroy();
-    }
-
-    private void StartAnimation()
-    {
-        body.localScale = Vector3.one;
-
-        if (isLockVisual)
-            return;
-        
-        var sequence = DOTween.Sequence().SetId(body);
-        SetCustomMaterial(BoardElementMaterialType.PiecesLowHighlightMaterial, true);
-        
-        sequence.SetLoops(-1);
-        
-        sequence.Append(body.DOScale(Vector3.one * 1.1f, 0.55f).SetEase(Ease.OutSine));
-        sequence.Append(body.DOScale(Vector3.one, 0.40f).SetEase(Ease.OutSine));
-        
-        sequence.Goto(Random.Range(0, 0.95f), true);
-    }
-
-    private void OnDisable()
-    {
-        DOTween.Kill(body);
-    }
-
-    public override void ToggleLockView(bool enabled)
-    {
-        base.ToggleLockView(enabled);
-        
-        StartAnimation();
-    }
+   
 }
