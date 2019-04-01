0e2c073476195cd6ad677c9198db09a3f2afbb26
OlegDolmatovih <oleg@neskinsoft.com>
2019-04-01 18:21:46 +0300

    up worker animation

--- Assets/Scripts/ProjectScripts/GameLogic/View/AnimationControllers/WorkerControllerAnimation.cs
+++ Assets/Scripts/ProjectScripts/GameLogic/View/AnimationControllers/WorkerControllerAnimation.cs
@@ -1,18 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class WorkerControllerAnimation : MonoBehaviour
+public class WorkerControllerAnimation : StateMachineBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private List<int> hashs = new List<int>
     {
-        
+        Animator.StringToHash("dance"),
+        Animator.StringToHash("breath"),
+        Animator.StringToHash("simple"),
+        Animator.StringToHash("breath 0")
+    };
+    
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        var id = hashs[Random.Range(0, hashs.Count)];
+
+        animator.Play(id, 0, Random.Range(0, 10) * 0.1f);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
 }
