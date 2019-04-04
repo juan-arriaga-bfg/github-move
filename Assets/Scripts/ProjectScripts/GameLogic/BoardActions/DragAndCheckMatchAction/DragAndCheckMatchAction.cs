47e710be4888602fb811699f56d2ec1a3eafee2e
DanilaZhelenok <daniel@neskinsoft.com>
2019-04-04 18:28:04 +0300

    up mine view

--- Assets/Scripts/ProjectScripts/GameLogic/BoardActions/DragAndCheckMatchAction/DragAndCheckMatchAction.cs
+++ Assets/Scripts/ProjectScripts/GameLogic/BoardActions/DragAndCheckMatchAction/DragAndCheckMatchAction.cs
@@ -417,13 +417,15 @@ public class DragAndCheckMatchAction : IBoardAction
 	private List<BoardPosition> NormalizeFreeList(List<BoardPosition> free, List<BoardPosition> piecesPositions)
 	{
 		var normalized = new List<BoardPosition>();
-		if (free == null || free.Count == 0)
-		{
-			return normalized;
-		}
+		
 		foreach (var piecePos in piecesPositions)
 		{
+			if (free.Count == 0)
+			{
+				break;
+			}
 			var near = free[0];
+			
 			var value = Math.Abs(free[0].X - piecePos.X) + Math.Abs(free[0].Y - piecePos.Y);
 			for (int i = 1; i < free.Count; i++)
 			{
--- Assets/Scripts/ProjectScripts/GameLogic/View/PieceView/MineBuildingPieceView.cs
+++ Assets/Scripts/ProjectScripts/GameLogic/View/PieceView/MineBuildingPieceView.cs
@@ -3,6 +3,59 @@ using UnityEngine;
 
 public class MineBuildingPieceView : BuildingPieceView
 {
+    private ParticleView workingParticle;
+    
+    public override void Init(BoardRenderer context, Piece piece)
+    {
+        base.Init(context, piece);
+
+        if (state != null)
+        {
+            state.OnChangeState += BuildAnimation;
+        }
+        
+        BuildAnimation();
+    }
+
+    public override void ResetViewOnDestroy()
+    {
+        base.ResetViewOnDestroy();
+        
+        if (state != null)
+        {
+            state.OnChangeState -= BuildAnimation;
+        }
+    }
+    
+    private void BuildAnimation()
+    {
+        if (state.State == BuildingState.InProgress && workingParticle == null)
+        {
+            StartAnimation();
+        }
+        else
+        {
+            CompleteAnimation();
+        }
+    }
+    
+    
+
+    private void StartAnimation()
+    {
+        workingParticle?.DestroyOnBoard();
+        
+        workingParticle = ParticleView.Show(R.MineProcessParticle, Piece.CachedPosition);
+        workingParticle.transform.SetParentAndReset(transform);
+        workingParticle.SyncRendererLayers(Piece.CachedPosition.SetZ(BoardLayer.FX.Layer));
+    }
+
+    private void CompleteAnimation()
+    {
+        workingParticle?.DestroyOnBoard();
+        workingParticle = null;
+    }
+
     public override void ToggleLockedState(bool isLocked)
     {
 	    // Do nothing
