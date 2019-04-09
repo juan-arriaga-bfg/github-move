1ce39fbfbbc8db9534cc0eb12b344495b4991da8
Alex Sorokin <alex@neskinsoft.com>
2019-04-09 15:48:48 +0300

    Fix re#2339 - [квесты] Разделить по типам фигурок (ветка A, D, G...) квест KillTree

--- Assets/Scripts/ProjectScripts/BusinessLogic/GameData/Quests/Tasks/TaskKillTreeEntity.cs
+++ Assets/Scripts/ProjectScripts/BusinessLogic/GameData/Quests/Tasks/TaskKillTreeEntity.cs
@@ -1,37 +1,10 @@
+[TaskHighlight(typeof(HighlightTaskTreeBranch))]
 [TaskHighlight(typeof(HighlightTaskAnyTree))]
 [TaskHighlight(typeof(HighlightTaskNextFog))]
-public class TaskKillTreeEntity : TaskEventCounterEntity
+public class TaskKillTreeEntity : TaskHitTreeEntity
 {
     public static readonly int ComponentGuid = ECSManager.GetNextGuid();
     public override int Guid => ComponentGuid;
 
     protected override int EventCode => GameEventsCodes.ObstacleKilled;
-    
-    public override void OnBoardEvent(int code, object context)
-    {
-        if (!IsInProgress())
-        {
-            return;
-        }
-
-        if (code != EventCode)
-        {
-            return;
-        }
-
-        ObstacleLifeComponent lifeCmp = context as ObstacleLifeComponent;
-
-        Piece piece = lifeCmp?.Context;
-        if (piece == null)
-        {
-            return;
-        }
-        
-        PieceTypeDef pieceTypeDef = PieceType.GetDefById(piece.PieceType);
-
-        if (pieceTypeDef.Filter.Has(PieceTypeFilter.Tree))
-        {
-            CurrentValue += 1;
-        }
-    }
 }
