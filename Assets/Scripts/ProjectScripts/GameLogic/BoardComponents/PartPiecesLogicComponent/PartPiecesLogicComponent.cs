ba86e9404a30a809d77e015430962ce85fdf901f
OlegDolmatovih <oleg@neskinsoft.com>
2019-04-12 19:41:31 +0300

    up monument match

--- Assets/Scripts/ProjectScripts/GameLogic/BoardComponents/PartPiecesLogicComponent/PartPiecesLogicComponent.cs
+++ Assets/Scripts/ProjectScripts/GameLogic/BoardComponents/PartPiecesLogicComponent/PartPiecesLogicComponent.cs
@@ -1,63 +1,63 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class PartDef
 {
-    public List<BoardPosition> Pattern;
+    public int Id = PieceType.None.Id;
+    public BoardPosition Key;
+    public List<BoardPosition> Main;
+    public readonly List<List<BoardPosition>> Second = new List<List<BoardPosition>>();
+    public bool IsFree => Second.Count == 0 && Main == null;
+
+    private PartPiecesLogicComponent context;
     public ViewDefinitionComponent ViewDefinition;
-}
-
-public class PartPiecesLogicComponent : IECSComponent
-{
-    public static readonly int ComponentGuid = ECSManager.GetNextGuid();
-    public int Guid => ComponentGuid;
-
-    private BoardController context;
     
-    private readonly Dictionary<BoardPosition, ViewDefinitionComponent> bubbles = new Dictionary<BoardPosition, ViewDefinitionComponent>();
-    private readonly Dictionary<BoardPosition, List<PartDef>> links = new Dictionary<BoardPosition, List<PartDef>>();
+    public void Init(BoardPosition key, PartPiecesLogicComponent logic)
+    {
+        Key = key;
+        context = logic;
+    }
     
-    public void OnRegisterEntity(ECSEntity entity)
+    public void Add(List<BoardPosition> option)
     {
-        context = entity as BoardController;
-    }
-
-    public void OnUnRegisterEntity(ECSEntity entity)
-    {
-    }
-
-    public void Add(List<BoardPosition> positions)
-    {
-        var index = positions.Count / 2;
-        var position = positions[index];
-        
-        if (bubbles.ContainsKey(position)) return;
-        
-        var piece = context.BoardLogic.GetPieceAt(position);
-        var pieceType = piece.PieceType;
-        
-        positions.Find(tempPosition =>
+        if (option[2].Equals(Key))
         {
-            pieceType = context.BoardLogic.GetPieceAt(tempPosition).PieceType;
-            return pieceType != PieceType.Boost_CR.Id;
-        });
-        
-        bubbles.Add(position, piece.ViewDefinition);
-
-        foreach (var key in positions)
-        {
-            if (links.TryGetValue(key, out var defs) == false)
-            {
-                defs = new List<PartDef>();
-                links.Add(key, defs);
-            }
-            
-            defs.Add(new PartDef{ViewDefinition = piece.ViewDefinition, Pattern = positions});
+            Main = option;
+            OpenBubble();
+            return;
         }
         
+        Second.Add(option);
+    }
+
+    public void Remove(List<BoardPosition> option)
+    {
+        if (option[2].Equals(Key))
+        {
+            var view = ViewDefinition.AddView(ViewType.Bubble) as BubbleView;
+            
+            view.Change(false);
+            
+            Main = null;
+            ViewDefinition = null;
+            Id = PieceType.None.Id;
+            return;
+        }
+        
+        Second.Remove(option);
+    }
+
+    private void OpenBubble()
+    {
+        if (Id != PieceType.None.Id) return;
+        
+        var piece = context.Context.BoardLogic.GetPieceAt(Key);
+        
+        Id = piece.PieceType != PieceType.Boost_CR.Id ? piece.PieceType : context.Context.BoardLogic.GetPieceAt(Main[0]).PieceType;
+        ViewDefinition = piece.ViewDefinition;
+        
         var view = piece.ViewDefinition.AddView(ViewType.Bubble) as BubbleView;
-        var lastId = context.BoardLogic.MatchDefinition.GetLast(pieceType);
+        var lastId = context.Context.BoardLogic.MatchDefinition.GetLast(Id);
         var def = GameDataService.Current.PiecesManager.GetPieceDef(lastId);
         
         var title = string.Format(LocalizationService.Get("gameboard.bubble.message.castle.build", "gameboard.bubble.message.castle.build\n{0}?"), DateTimeExtension.GetDelayText(def.Delay, true));
@@ -67,85 +67,93 @@ public class PartPiecesLogicComponent : IECSComponent
         view.Change(true);
     }
     
-    public void Remove(BoardPosition position)
-    {
-        if (links.TryGetValue(position, out var defs) == false) return;
-        
-        if (defs.Count == 0)
-        {
-            links.Remove(position);
-            return;
-        }
-        
-        var partDef = new List<PartDef>(defs);
-
-        foreach (var def in partDef)
-        {
-            Remove(def.Pattern);
-        }
-    }
-
-    public void Remove(List<BoardPosition> positions)
-    {
-        var index = positions.Count / 2;
-        var position = positions[index];
-        
-        if (bubbles.TryGetValue(position, out var view) == false) return;
-
-        foreach (var key in positions)
-        {
-            if (links.TryGetValue(key, out var defs) == false) continue;
-
-            var partDef = defs.Find(def => def.ViewDefinition == view);
-
-            defs.Remove(partDef);
-            
-            if (defs.Count == 0) links.Remove(key);
-        }
-        
-        bubbles.Remove(position);
-        view.AddView(ViewType.Bubble).Change(false);
-    }
-    
     private void OnClick(Piece piece)
     {
-        if (context.WorkerLogic.Get(piece.CachedPosition, null) == false) return;
-        Work(piece, false);
+        if (context.Context.WorkerLogic.Get(piece.CachedPosition, null) == false) return;
+        context.Work(piece.CachedPosition, false);
+    }
+}
+
+public class PartPiecesLogicComponent : IECSComponent
+{
+    public static readonly int ComponentGuid = ECSManager.GetNextGuid();
+    public int Guid => ComponentGuid;
+
+    public BoardController Context;
+    
+    private readonly Dictionary<BoardPosition, PartDef> links = new Dictionary<BoardPosition, PartDef>();
+    
+    public void OnRegisterEntity(ECSEntity entity)
+    {
+        Context = entity as BoardController;
     }
 
-    public bool Work(Piece piece, bool isExtra)
+    public void OnUnRegisterEntity(ECSEntity entity)
     {
-        var key = piece.PieceType;
-        var pattern = new List<BoardPosition>();
-        
-        if (key == PieceType.Boost_CR.Id)
-        {
-            var def = links[piece.CachedPosition].Find(d => d.ViewDefinition == piece.ViewDefinition);
+    }
 
-            pattern = def.Pattern;
-            
-            foreach (var position in pattern)
+    public void Add(List<BoardPosition> positions)
+    {
+        foreach (var key in positions)
+        {
+            if (links.TryGetValue(key, out var def) == false)
             {
-                var id = piece.Context.BoardLogic.GetPieceAt(position).PieceType;
-                
-                if (id == PieceType.Boost_CR.Id) continue;
-                
-                key = id;
-                break;
+                def = new PartDef();
+                def.Init(key, this);
+                links.Add(key, def);
             }
+            
+            def.Add(positions);
+        }
+    }
+    
+    public void Remove(BoardPosition position)
+    {
+        if (links.TryGetValue(position, out var def) == false) return;
+        
+        if (def.Main != null)
+        {
+            Remove(def.Main, position);
+            def.Remove(def.Main);
+        }
+
+        for (var i = def.Second.Count - 1; i >= 0; i--)
+        {
+            var option = def.Second[i];
+            Remove(option, position);
+            def.Remove(option);
         }
         
+        links.Remove(position);
+    }
+
+    private void Remove(List<BoardPosition> positions, BoardPosition ignore)
+    {
+        foreach (var pos in positions)
+        {
+            if (ignore.Equals(pos) || links.TryGetValue(pos, out var child) == false) continue;
+                
+            child.Remove(positions);
+            
+            if (child.IsFree) links.Remove(pos);
+        }
+    }
+    
+    public bool Work(BoardPosition position, bool isExtra)
+    {
+        if (links.TryGetValue(position, out var def) == false) return false;
+        
         var positions = new List<BoardPosition>();
-        var action = context.BoardLogic.MatchActionBuilder.GetMatchAction(positions, key, piece.CachedPosition);
+        var action = Context.BoardLogic.MatchActionBuilder.GetMatchAction(positions, def.Id, position);
         
         if (action == null) return false;
         
-        Remove(positions);
-        context.ActionExecutor.PerformAction(action);
+        Remove(position);
+        Context.ActionExecutor.PerformAction(action);
 
         if (!isExtra) return true;
         
-        var result = context.BoardLogic.GetPieceAt(positions[0])?.PieceState;
+        var result = Context.BoardLogic.GetPieceAt(positions[0])?.PieceState;
             
         result?.Timer.Subtract(GameDataService.Current.ConstantsManager.ExtraWorkerDelay);
 
@@ -154,7 +162,16 @@ public class PartPiecesLogicComponent : IECSComponent
 
     public List<ViewDefinitionComponent> GetAllView()
     {
-        return bubbles.Values.ToList();
+        var result = new List<ViewDefinitionComponent>();
+
+        foreach (var value in links.Values)
+        {
+            if (value.ViewDefinition == null) continue;
+            
+            result.Add(value.ViewDefinition);
+        }
+
+        return result;
     }
     
     public List<BoardPosition> GetAllPositions()
@@ -163,10 +180,7 @@ public class PartPiecesLogicComponent : IECSComponent
 
         foreach (var value in links.Values)
         {
-            foreach (var def in value)
-            {
-                result.AddRange(def.Pattern);
-            }
+            result.Add(value.Key);
         }
 
         return result;
