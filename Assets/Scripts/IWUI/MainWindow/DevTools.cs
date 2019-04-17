4859e1ee65bb799ab2626fe177713400da84c32a
Alex Sorokin <alex@neskinsoft.com>
2019-04-17 15:28:04 +0300

    Airship sorting up

--- Assets/Scripts/IWUI/MainWindow/DevTools.cs
+++ Assets/Scripts/IWUI/MainWindow/DevTools.cs
@@ -143,7 +143,18 @@ public class DevTools : UIContainerElementViewController
     public void OnSpawnAirShipClick()
     {
         Debug.Log("OnSpawnAirShipClick");
-
+        //
+        // var pieces1 = new Dictionary<int, int>
+        // {
+        //     { PieceType.Parse("A1"), 1 },
+        //     { PieceType.Parse("B3"), 1 },
+        //     { PieceType.Parse("A2"), 1 },
+        //     { PieceType.Parse("B5"), 1 },
+        // };
+        //
+        // BoardService.Current.FirstBoard.BoardLogic.AirShipLogic.Add(pieces1);        
+        // return;
+        
         var allIds = PieceType.GetAllIds();
         
         for (int i = allIds.Count - 1; i >=0 ; i--)
