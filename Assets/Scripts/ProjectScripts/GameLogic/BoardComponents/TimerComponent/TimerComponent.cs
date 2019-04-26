cb82c45c56ae40e2cfba6318c4aee88ab162500c
OlegDolmatovih <oleg@neskinsoft.com>
2019-04-26 12:05:56 +0300

    revert fix timer

--- Assets/Scripts/ProjectScripts/GameLogic/BoardComponents/TimerComponent/TimerComponent.cs
+++ Assets/Scripts/ProjectScripts/GameLogic/BoardComponents/TimerComponent/TimerComponent.cs
@@ -230,7 +230,6 @@ public class TimerComponent : IECSComponent, IECSSystem, ITimerComponent
     
     public bool IsFree()
     {
-        var start = (int) StartTime.GetTime(UseUTC).TotalSeconds;
-        return start > 0 && start < Delay && CompleteTime.GetTimeLeft(UseUTC).TotalSeconds <= GameDataService.Current.ConstantsManager.FreeTimeLimit;
+        return (int) StartTime.GetTime(UseUTC).TotalSeconds < Delay && CompleteTime.GetTimeLeft(UseUTC).TotalSeconds <= GameDataService.Current.ConstantsManager.FreeTimeLimit;
     }
 }
