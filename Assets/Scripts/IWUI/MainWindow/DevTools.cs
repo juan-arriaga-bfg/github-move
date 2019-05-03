cfedc726cc43162725381dd4887a80290c321cba
Maria.Verinskaya <maria@neskinsoft.com>
2019-05-03 16:31:59 +0300

    fixed "Debug2" button

--- Assets/Scripts/IWUI/MainWindow/DevTools.cs
+++ Assets/Scripts/IWUI/MainWindow/DevTools.cs
@@ -382,7 +382,6 @@ public class DevTools : UIContainerElementViewController
     public void OnDebug2Click()
     {
         Debug.Log("OnDebug2Click");
-        return;
         
         GameDataService.Current.QuestsManager.StartNewDailyQuest();
         return;
