2f994c81ae33068b0349d5073d0300975311a039
Alex Sorokin <alex@neskinsoft.com>
2019-05-09 16:55:45 +0300

    Analytics helper tool added

---
+++ Misc/AnalyticsMonitor/routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var dataParser = require('sources/dataParser');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Analytics monitor' });
+});
+
+router.get('/hacked-analytics/*', function(req, res, next) {
+    dataParser.add(req.url);
+    res.status(200).send("OK");
+});
+
+router.get('/debug', function(req, res, next) {
+    var text = dataParser.debug();
+    res.status(200).send(text);
+});
+
+router.get('/data', function(req, res) {
+    var id = req.param('id');
+    var data = dataParser.get(id);
+    res.status(200).send(data);
+});
+
+module.exports = router;
