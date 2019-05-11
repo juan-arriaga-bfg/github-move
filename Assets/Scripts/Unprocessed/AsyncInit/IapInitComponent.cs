0175db45111681335d4f1fc608be5ee93ad6c4d0
DanilaZhelenok <daniel@neskinsoft.com>
2019-05-11 16:23:44 +0300

    set default iap values (re#2365)

--- Assets/Scripts/Unprocessed/AsyncInit/IapInitComponent.cs
+++ Assets/Scripts/Unprocessed/AsyncInit/IapInitComponent.cs
@@ -36,77 +36,88 @@ public class IapInitComponent : AsyncInitComponentBase
                                      Id = "iap1",
                                      GooglePlayId     = "com.bigfishgames.mergetalesgoog.tier2",
                                      AppleAppStoreId  = "vi.tier2.com.bigfishgames.mergetalesios",
-                                     Consumable = true
+                                     Consumable = true,
+                                     DefaultPrice = "???"
                                  })
                                 .Add(new IapDefinition
                                  {
                                      Id = "iap2",
                                      GooglePlayId     = "com.bigfishgames.mergetalesgoog.tier5",
                                      AppleAppStoreId  = "vi.tier5.com.bigfishgames.mergetalesios",
-                                     Consumable = true
+                                     Consumable = true,
+                                     DefaultPrice = "???"
                                  })
                                 .Add(new IapDefinition
                                  {
                                      Id = "iap3",
                                      GooglePlayId     = "com.bigfishgames.mergetalesgoog.tier10",
                                      AppleAppStoreId  = "vi.tier10.com.bigfishgames.mergetalesios",
-                                     Consumable = true
+                                     Consumable = true,
+                                     DefaultPrice = "???"
                                  })
                                 .Add(new IapDefinition
                                  {
                                      Id = "iap4",
                                      GooglePlayId     = "com.bigfishgames.mergetalesgoog.tier20",
                                      AppleAppStoreId  = "vi.tier20.com.bigfishgames.mergetalesios",
-                                     Consumable = true
+                                     Consumable = true,
+                                     DefaultPrice = "???"
                                  })
                                 .Add(new IapDefinition
                                  {
                                      Id = "iap5",
                                      GooglePlayId     = "com.bigfishgames.mergetalesgoog.tier50",
                                      AppleAppStoreId  = "vi.tier50.com.bigfishgames.mergetalesios",
-                                     Consumable = true
+                                     Consumable = true,
+                                     DefaultPrice = "???"
                                  })
                                 .Add(new IapDefinition
                                  {
                                      Id = "iap6",
                                      GooglePlayId     = "com.bigfishgames.mergetalesgoog.tier60",
                                      AppleAppStoreId  = "vi.tier60.com.bigfishgames.mergetalesios",
-                                     Consumable = true
+                                     Consumable = true,
+                                     DefaultPrice = "???"
                                  })
                                 .Add(new IapDefinition
                                  {
                                      Id = "iap8",
                                      GooglePlayId     = "com.bigfishgames.mergetalesgoog.bundletier5",
                                      AppleAppStoreId  = "vi.bundletier5.com.bigfishgames.mergetalesios",
-                                     Consumable = true
+                                     Consumable = true,
+                                     DefaultPrice = "???"
                                  })
                                 .Add(new IapDefinition
                                  { 
                                     Id = "iap9",
                                     GooglePlayId     = "com.bigfishgames.mergetalesgoog.bundletier7",
                                     AppleAppStoreId  = "vi.bundletier7.com.bigfishgames.mergetalesios",
-                                    Consumable = true
+                                    Consumable = true,
+                                    DefaultPrice = "???"
                                  })
                                 .Add(new IapDefinition
                                  {
                                      Id = "iap10",
                                      GooglePlayId     = "com.bigfishgames.mergetalesgoog.bundletier10",
                                      AppleAppStoreId  = "vi.bundletier10.com.bigfishgames.mergetalesios",
-                                     Consumable = true
+                                     Consumable = true,
+                                     DefaultPrice = "???"
                                  })
                                 .Add(new IapDefinition
                                  {
                                     Id = "iap11",
                                     GooglePlayId     = "com.bigfishgames.mergetalesgoog.bundletier15",
                                     AppleAppStoreId  = "vi.bundletier15.com.bigfishgames.mergetalesios",
-                                    Consumable = true
+                                    Consumable = true,
+                                    DefaultPrice = "???"
                                  })
                                 .Add(new IapDefinition                                 
                                 {
                                      Id = "iap12",
                                      GooglePlayId     = "com.bigfishgames.mergetalesgoog.bundletier20",
                                      AppleAppStoreId  = "vi.bundletier20.com.bigfishgames.mergetalesios",
-                                     Consumable = true
+                                     Consumable = true,
+                                     DefaultPrice = "???"
                                  })
                 ))
 #endregion
